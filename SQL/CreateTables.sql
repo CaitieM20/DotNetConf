e2cf0cd8eab4a176838fa92feddc98dabcce46bd
Caitlin <caitiem20@gmail.com>
2013-04-22 21:20:10 -0700

    Sql script update
    to include game table

--- SQL/CreateTables.sql
+++ SQL/CreateTables.sql
@@ -2,7 +2,7 @@ CREATE TABLE [simple_stats].[dbo].[PlayerTable](
 
 	[player_id] [bigint] NOT NULL,
 	[player_name] [varchar](50) NOT NULL,
-	[player_pic] [image] NULL
+	[player_pic] [image] NULL,
 
 	PRIMARY KEY (player_id)
 )
@@ -15,21 +15,31 @@ CREATE TABLE [simple_stats].[dbo].[PlayerStats](
 	[total_points] [int],
 	[total_kills] [int],
 	[total_deaths] [int],
-	[total_timePlayed] [bigint]
-	PRIMARY KEY (player_id)
+	[total_timePlayed] [bigint],
+	PRIMARY KEY (player_id),
 	FOREIGN KEY (player_id) REFERENCES PlayerTable(player_id)
 )
 GO
 
+Create Table [simple_stats].[dbo].[Games](
+	[game_id] [bigint] NOT NULL,
+	[game_duration] [int],
+	PRIMARY KEY (game_id)
+)
+GO
+
 CREATE TABLE [simple_stats].[dbo].[PlayerGames](
-	[player_id] [bigint] NOT NULL,
+	[player_id] [bigint] NOT NULL ,
 	[game_id] [bigint] NOT NULL,
 	[win] [bit],
 	[points_earned] [int],
 	[kills] [int],
 	[deaths] [int],
-	[game_duration] [int]
-	PRIMARY KEY (player_id, game_id)
-	FOREIGN KEY (player_id) REFERENCES PlayerTable(player_id)
+	PRIMARY KEY (player_id, game_id),
+	FOREIGN KEY (player_id) REFERENCES PlayerTable(player_id),
+	FOREIGN KEY (game_id) REFERENCES Games(game_id)
 )
 GO
+
+
+
